076e61d38f3409215cf03130d8b6ec032741fdae
Yanatchara JERAJA <yanatchara.j@ku.th>
2024-04-23 13:52:50 +0700

    add path

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import HomePage from './pages/homepage';
 import Prediction from './pages/prediction';
+import Statistics from './pages/statistics';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/prediction" element={<Prediction />} />
+        <Route path="/statistics" element={<Statistics />} />
       </Routes>
     </Router>
   );
